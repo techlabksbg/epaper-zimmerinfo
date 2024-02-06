bec9d1e68bb50d208ebcac7c7a7115fa4f3ce9ce
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2024-02-06 09:55:54 +0100

    heavy lifting done

--- basics/webcrawl/crawler.py
+++ basics/webcrawl/crawler.py
@@ -11,29 +11,26 @@ def clearCookies():
         os.remove("nesa-cookies.txt")
 
 
+# replacements is of the form [["ALT", "neu"], ["uralt", "brandneu"]]
 def applyAllReplacements(s, repl):
     for r in repl:
         s = s.replace(r[0], r[1])
     return s
 
 # replacements is of the form [["ALT", "neu"], ["uralt", "brandneu"]]
-
 def execCurl(datei, replacements=[]):
     with open(datei, "r") as f:
         args = [a for a in shlex.split(f.read()) if a!="\n"]  # Split file into arguments like the shell would (well, except for escaped new lines, it seems)
     
     cookieHandling = shlex.split("-L -b nesa-cookies.txt -c nesa-cookies.txt")
     args = args[0:1] + cookieHandling + args[1:]
-    print(args.__repr__())
-    print(replacements.__repr__())
     if len(replacements)>0:
         args = [applyAllReplacements(a, replacements) for a in args]
     
-    print(args.__repr__())
     # from https://stackoverflow.com/questions/4760215/running-shell-command-and-capturing-the-output
     result = subprocess.run(args, stdout=subprocess.PIPE)
     htmlcode = result.stdout.decode('utf-8')
-    tree = html.fromstring(htmlcode)
+    tree = html.fromstring(htmlcode)  # Build an lxml-tree for easy access to element via XPath
     return tree
 
 
@@ -51,7 +48,17 @@ def performLogin():
     return execCurl("login.curl", r)
 
 
-s = performLogin()
+# Use cached page to avoid too many requests
+if os.path.exists("home.html"):
+    with open("home.html", "r") as f:
+        s = html.parse("home.html")
+else:
+    s = performLogin()
+    with open("home.html", "wb") as f:
+        f.write(html.tostring(s))
 
-
-
+for a in s.findall(".//nav/a"):
+    d = a.find("./div")
+    if d!=None:
+        print(a.attrib['href'])
+        print(d.text)
