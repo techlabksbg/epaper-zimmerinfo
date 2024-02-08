0db0002f9d5788c3ca8665238939726579dcdca8
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2024-02-08 11:41:38 +0100

    extractor

--- basics/webcrawl/raumplanextractor.py
+++ basics/webcrawl/raumplanextractor.py
@@ -10,7 +10,13 @@ tree = html.parse(datei)
 # Eintrag:
 # /html/body/div[4]/div/main/div/div/main/div/div[4]/div[5]/div[1]/div[1]
 # //*[@id="scheduler"]/div[5]
-for a in tree.findall(".//*[@event_id]"):
-    print(a)
-    d = a.find(".//div[@class='stpt_event_title']")
-    print(d.attrib['title'])
+
+cal = tree.find(".//*[@class='dhx_cal_data']")
+for day in cal.findall(".//*[@data-column-index]"):
+    datum = day.attrib['aria-label']
+    print(datum)
+    for event in day.findall(".//*[@event_id]"):
+        d = event.find(".//div[@class='stpt_event_title']")
+        print(datum)
+        print(d.attrib['title'])
+
