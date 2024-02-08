60356a11bc9db763f3800f6237852f4d2f54f501
Florian <florian.glaus@hotmail.com>
2024-02-08 12:09:16 +0100

    Kann jtzt ical einlesen und printen

--- basics/ics/reservations-discriminator.py
+++ basics/ics/reservations-discriminator.py
@@ -4,8 +4,18 @@ from datetime import datetime
 filepath = "h21.ics"
 
 with open(filepath, "rb") as file:
-    calendar = file.read()
+    calendar = file.read().decode('utf-8')
 
 
-timetable = icalendar.Calendar.from_ical(calendar)
-print (timetable)
+# timetable = icalendar.Calendar.from_ical(calendar)
+# print (timetable)
+
+event = icalendar.Calendar.from_ical(calendar)
+for event in event.walk():
+    name = event.get('SUMMARY')
+    start_data = event.get('DTSTART')
+    end_date = event.get('DTEND')
+    print ("Name:", name, "| Start Date:", start_data, "| End Date:", end_date)
+# start_date = event.get('DTSTART').date()
+# end_date = event.get('DTEND').dt.date()
+
