d7b03ae500249867f4a3a09e0dc2469f2b3be3c1
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2024-02-15 12:09:09 +0100

    Ausgabe auch auf Deutsch

--- basics/xml2graphics/day.py
+++ basics/xml2graphics/day.py
@@ -1,8 +1,19 @@
-
+import deutsch
 
 class Day:
-    def __init__(self):
+    def __init__(self, date):
         self.events = []
+        self.date = date
 
     def add(self, event):
+        if self.date != event.start_datetime.date():
+            raise RuntimeError("Datum nicht gleich!")
         self.events.append(event)
+        self.events.sort(key = lambda e : e.start_datetime)
+
+    def ausgabe(self):
+        print(deutsch.datum(self.date))
+        print("="*30)
+        for event in self.events:
+            print(event)
+        print("-"*20)
--- basics/xml2graphics/xmlPlanParser.py
+++ basics/xml2graphics/xmlPlanParser.py
@@ -1,11 +1,14 @@
 from lxml import etree
 from event import Event
-
+from week import Week
+from datetime import date
 tree = etree.parse("H21.xml")
 
 events = []
 for event in tree.findall(".//event"):
     events.append(Event(event))
     
-events.sort(key= lambda e : e.start_datetime)
-print([str(e) for e in events])
+
+week = Week(events, date.today())
+
+week.ausgabe()
