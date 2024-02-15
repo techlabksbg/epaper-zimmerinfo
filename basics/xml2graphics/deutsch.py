b4525cbb201460da1db17ac46cd856ad6021b0b6
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2024-02-15 12:09:09 +0100

    Ausgabe auch auf Deutsch

---
+++ basics/xml2graphics/deutsch.py
@@ -0,0 +1,12 @@
+wochentage = ['Montag', 'Dienstag', 'Mittwoch', "Donnerstag", 'Freitag', 'Samstag', 'Sonntag']
+def wochentag(datum):
+    return wochentage[datum.weekday()]
+
+monate = ['', 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'Oktober', 'November', 'Dezember']
+def monat(datum):
+    return monate[datum.month]
+
+def datum(dat):
+    return f"{wochentag(dat)} {dat.day}. {monat(dat)} {dat.year}"
+
+
--- basics/xml2graphics/week.py
+++ basics/xml2graphics/week.py
@@ -2,7 +2,7 @@ from datetime import date, datetime, timedelta
 from day import Day
 class Week:
     def __init__(self, events, start_date):
-        self.days = [Day() for i in range(7)]
+        self.days = [Day(start_date + timedelta(days=i)) for i in range(7)]
         self.start_date = start_date
         self.make_days(events)
 
@@ -14,3 +14,8 @@ class Week:
                 self.days[index].add(event)
             
 
+    def ausgabe(self):
+        print(f"Woche vom {self.start_date}")
+        print("#"*30)
+        for day in self.days:
+            day.ausgabe()
