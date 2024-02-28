3cc5718a398969e8952c91fb86859f1a36c9508d
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2024-02-28 15:54:19 +0100

    unconvincing experiments

--- basics/cairo/stdplan.py
+++ basics/cairo/stdplan.py
@@ -13,9 +13,13 @@ for i in range(0,480,48):
     ctx.line_to(799.5,i-0.5)
     ctx.stroke()
 
+notperfect = ["PixelPerfect"]
+fonts = [ "Pixel Emulator", "High Pixel\-7", "Thirteen Pixel Fonts", "Mini Pixel\-7", "Minimal5x7", ]
 
-ctx.select_font_face("Minimal5x7")
-ctx.set_font_size(16)
-ctx.move_to(10, 80)
-ctx.show_text(f"Hello mit Schrift Minimal5x7 mit Grösse 16 être à blà blé maïs...")
+for fn,font in enumerate(fonts):
+    ctx.select_font_face(font)
+    for s in range(10,22,1):
+        ctx.set_font_size(s)
+        ctx.move_to(10+180*fn, s*20-160)
+        ctx.show_text(f"#{font} s={s}")
 surface.write_to_png("stdplan.png")  # Output to PNG
