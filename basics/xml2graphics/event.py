5e1ddc2c73868ea90c07259377f70a7944ad72a7
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2024-02-29 11:59:21 +0100

    mehr daten in event

--- basics/xml2graphics/event.py
+++ basics/xml2graphics/event.py
@@ -7,7 +7,14 @@ class Event:
         self.end_datetime = datetime.strptime(end_date, '%Y-%m-%d %H:%M')
         self.text = tree.find('.//text').text
         self.color = tree.find('.//color').text
+        self.klasse = tree.find('.//klasse').text
+        if self.klasse:
+            self.klassekurz = self.klasse.replace("*","")
+        self.fachkuerzel = tree.find('.//fachkuerzel').text
         self.lehrerkuerzelname = tree.find('.//lehrerkuerzelname').text
+        if self.lehrerkuerzelname:
+            self.lehrername = self.lehrerkuerzelname[0:self.lehrerkuerzelname.find("(")]
+        self.lehrerkuerzel = tree.find('.//lehrerkuerzel').text
         self.reservator = tree.find('.//reservator').text
 
         if (self.start_datetime>self.end_datetime):
