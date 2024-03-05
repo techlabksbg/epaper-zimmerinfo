3429430a96765c029255fde489a1e55e92c618c1
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2024-03-05 15:30:45 +0100

    lehrernamen kuerzen

--- basics/xml2graphics/deutsch.py
+++ basics/xml2graphics/deutsch.py
@@ -3,10 +3,21 @@ def wochentag(datum):
     return wochentage[datum.weekday()]
 
 monate = ['', 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'Oktober', 'November', 'Dezember']
-def monat(datum):
+def monat(datum) -> str:
     return monate[datum.month]
 
-def datum(dat):
+def datum(dat) -> str:
     return f"{wochentag(dat)} {dat.day}. {monat(dat)} {dat.year}"
 
 
+def lehrerName(name:str, level:int = 0) -> str:
+    name = name.strip()
+    print(f"->{name}<-")
+    if (level==0):
+        return name
+    if (level==1):
+        el = name.split(" ")
+        print(el)
+        return " ".join(el[0:-1])+f" {el[-1][0]}."
+    if (level>1):
+        return name[0:-level]+"..."
