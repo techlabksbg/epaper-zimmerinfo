4cf03471c3280fa88ccd72b1a9a4d80fc3cda28f
Jonas Gassmann <jonas.g.g@icloud.com>
2024-03-11 17:18:44 +0100

    WiFi

--- basics/esp32/WiFi/src/main.cpp
+++ basics/esp32/WiFi/src/main.cpp
@@ -1,11 +1,23 @@
 #include <Arduino.h>
 #include <WiFi.h>
-
 #include "secrets.h"
+// Also see https://randomnerdtutorials.com/esp32-adc-analog-read-arduino-ide/
+#define BATPIN 34
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASSWD);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
 
 void setup(){
-    WiFi.disconnect();
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(SSID, PASSWD);
-    Serial.print("Connecting to WiFi ..");
+  Serial.begin(115200);
+  initWiFi();
 }
+
+void loop(){}
