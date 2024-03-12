fd180734de47cb24abc9a054916864b5007ab2d1
pizzamoeger <osshannahsophie@gmail.com>
2024-03-12 15:07:36 +0100

    calculating sleep time

---
+++ basics/server/flaskr/misc.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+now = datetime.now()
+
+six = now.replace(hour=6, minute=0, second=0, microsecond=0)
+ten = now.replace(hour=10, minute=0, second=0, microsecond=0)
+twelve = now.replace(hour=12, minute=0, second=0, microsecond=0)
+seventeen = now.replace(hour=17, minute=0, second=0, microsecond=0)
+
+times = [six, ten, twelve, seventeen]
+
