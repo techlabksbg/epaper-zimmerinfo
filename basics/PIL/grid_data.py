4b09f785fcd146875bf223bf40e037a56bf1e8b3
Florian <florian.glaus@hotmail.com>
2024-03-12 15:00:44 +0100

    will einf nicht

--- basics/PIL/grid_data.py
+++ basics/PIL/grid_data.py
@@ -1,4 +1,7 @@
-AnfangszeitenKSBG = [
+from operator import mul
+from PIL import Image, ImageFont, ImageDraw, ImageColor
+
+AnfangszeitenKSBG = {
     "07:40" : 48,
     "08:34" : 74,
     "09.28" : 103,
@@ -15,23 +18,54 @@ AnfangszeitenKSBG = [
     "18:15" : 393,
     "19:05" : 422,
     "19:55" : 451
-]
+}
 
-Wochentage = [
+Wochentage = {
     "Dienstag" : 246,
     "Mittwoch" : 358,
     "Donnerstag" : 470,
     "Freitag" : 582,
     "Samstag" : 694
-]
+}
 
 
+def minutenrechner(time):
+    my_time = time
+    factors = (60, 1, 1/60)
 
+    t1 = sum(i*j for i, j in zip(map(int, my_time.split(':')), factors))
+    
+    print(t1)
+    return t1
+
+minutenrechner("17:40")
+
+def koordinatenfinder(timein):
+    if timein in AnfangszeitenKSBG:
+        return AnfangszeitenKSBG[timein]
+
+#     for time in AnfangszeitenKSBG.keys():
+#         x = minutenrechner(timein)
+#         y = minutenrechner(AnfangszeitenKSBG.keys())
+#         if x == y:
+#             return y[x]
+#             break
+#         # else 
+
+def hauptsacheeinrechteck(timea, timeb):
+        #img = Image.new("RGB", (800,480), color=(255,255,255))
+    bw = Image.new("1", (800,480), color=1)
+
+    #font = ImageFont.truetype("../pixelperfect/Minimal5x7.ttf", size=32)
+    drawbw = ImageDraw.Draw(bw)
+    drawbw.rounded_rectangle([(43, koordinatenfinder(timea)), (240, koordinatenfinder(timeb))] , fill ="white", radius=1, outline ="black", width = 1) 
+    bw.show()
 
 def draw_lesson(subject_short, Class, teacher_short, aditional_info, time, day):
     info = subject_short + " " + Class
     draw_point = (Wochentage[day], AnfangszeitenKSBG[time])
     drawbw.multiline_text(draw_point, text=info, font=font, fill=0)
+
     info = teacher_short + " " + aditional_info
     draw_point = (Weekdays[day], AnfangszeitenKSBG[time]+14)
     drawbw.multiline_text(draw_point, text=info, font=font, fill=0)
