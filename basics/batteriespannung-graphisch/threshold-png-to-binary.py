9663b400986209086930ee319775eba2a94037b2
Olaf <olaf.schnuerer@ksbg.ch>
2024-03-12 14:43:34 +0100

    threshold for binary

--- basics/batteriespannung-graphisch/threshold-png-to-binary.py
+++ basics/batteriespannung-graphisch/threshold-png-to-binary.py
@@ -4,29 +4,40 @@ import cv2
 import numpy as np
 import math
 
-########## IM WESENTLICHEN KOPIE von ../dithering/dither.py ######################
+########## EINIGES KOPIERT von ../dithering/dither.py ######################
 
 def readAndConvertRGB(file):
     return cv2.imread(file)/255  # Conversion to float in 0..1
 
 
-def ditherWithRed(img):
-    positions = [[1,0],[-1,1],[0,1],[1,1]]
-    weights = [7/16, 3/16, 5/16, 1/16]
-    colors = np.array([[0,0,0], [0,0,1], [1,1,1]])
+# def ditherWithRed(img):
+#     positions = [[1,0],[-1,1],[0,1],[1,1]]
+#     weights = [7/16, 3/16, 5/16, 1/16]
+#     colors = np.array([[0,0,0], [0,0,1], [1,1,1]])
+#     c = np.copy(img)
+#     h,w,_ = c.shape
+#     for y in range(h):
+#         for x in range(w):
+#             d = [np.linalg.norm(c[y,x]-cc) for cc in colors]
+#             i = d.index(min(d))
+#             c[y,x] = colors[i]
+#             e = img[y,x]-c[y,x]
+#             for i in range(len(positions)):
+#                 a = x+positions[i][0]
+#                 b = y+positions[i][1]
+#                 if (a>=0 and a<w and b<h):
+#                     c[b,a]+=e*weights[i]
+#     return c
+
+def filter(img):
+    # positions = [[1,0],[-1,1],[0,1],[1,1]]
+    # weights = [7/16, 3/16, 5/16, 1/16]
+    # colors = np.array([[0,0,0], [0,0,1], [1,1,1]])
     c = np.copy(img)
     h,w,_ = c.shape
     for y in range(h):
         for x in range(w):
-            d = [np.linalg.norm(c[y,x]-cc) for cc in colors]
-            i = d.index(min(d))
-            c[y,x] = colors[i]
-            e = img[y,x]-c[y,x]
-            for i in range(len(positions)):
-                a = x+positions[i][0]
-                b = y+positions[i][1]
-                if (a>=0 and a<w and b<h):
-                    c[b,a]+=e*weights[i]
+            c[y,x] = [0, 0, 0] if np.sum(c[y,x]) < 1.5 else [1, 1, 1] 
     return c
 
 def toBinaryRB(tox,toy,img):
@@ -68,7 +79,8 @@ for datei in ['diagram', 'matplotdiagram-800x480']:
     print(img.shape)
     # cv2.imshow('Image', img)
     # cv2.waitKey(100)
-    c = ditherWithRed(img)
+    # c = ditherWithRed(img)
+    c = filter(img)
     # cv2.imshow('Image', c)
     # cv2.waitKey(1000)
     b, r = toBinaryRB(0, 0, c)
