8f2313c8873eedc07e3a4c0b1f70a7ea93518c73
pizzamoeger <osshannahsophie@gmail.com>
2024-03-12 15:56:30 +0100

    battery plotting

--- basics/server/flaskr/anzeige.py
+++ basics/server/flaskr/anzeige.py
@@ -7,9 +7,33 @@ from datetime import datetime
 from flaskr.auth import login_required
 from flaskr.db import get_db
 from flaskr.misc import times
+from flaskr.plotting import plot_voltage
+from flaskr.convert_to_bin import convert_to_bin
 from flask import request
 
+import os
+
 bp = Blueprint('anzeige', __name__)
+
+def calculate_sleep_time():
+    current_time = datetime.now()
+    sleep_time = 0
+    for time in times:
+        if (current_time < time):
+            sleep_time = (time - current_time).seconds
+            break
+
+    return sleep_time
+
+def plot_graph(macid):
+    path = f"flaskr/static/macs/{macid}"
+
+    # remove all files in the directory
+    for file in os.listdir(path):
+        os.remove(f"{path}/{file}")
+
+    plot_voltage(path, macid)
+    convert_to_bin(path)
 
 @bp.route('/anzeige')
 def index():
@@ -34,13 +58,11 @@ def index():
     if (roomid != None):
         roomid = roomid[0]
 
-    current_time = datetime.now()
-    sleep_time = 0
-    for time in times:
-        if (current_time < time):
-            sleep_time = (time - current_time).seconds
-            break
-
+    sleep_time = calculate_sleep_time()
     sleep_time = 120 # for testing purposes
+
+    # generate graph if needed
+    if (roomid == None):
+        plot_graph(macid)
 
     return render_template('anzeige/index.html', roomid=roomid, macid=macid, sleep_time=sleep_time)
