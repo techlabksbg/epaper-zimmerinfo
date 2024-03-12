365c84d8bc68a419a53338d86aaa7577d8fedc5e
Felix <bergmann.felix@gmx.ch>
2024-03-12 09:14:36 +0100

    wifi

--- basics/esp32/WiFi/src/main.cpp
+++ basics/esp32/WiFi/src/main.cpp
@@ -5,9 +5,10 @@
 #include "EPD.h"
 #include "GUI_Paint.h"
 #include <stdlib.h>
-#define nocon 0
+int nocon=0;
 // Also see https://randomnerdtutorials.com/esp32-adc-analog-read-arduino-ide/
-#define BATPIN 34
+#define BATPIN 34;
+#define MAC WiFi.macAddress();
 
 void initWiFi() {
   WiFi.mode(WIFI_STA);
@@ -23,10 +24,27 @@ void initWiFi() {
 
 void setup(){
   Serial.begin(115200);
+  UBYTE *BlackImage, *RYImage;
+  UWORD Imagesize = ((EPD_7IN5B_V2_WIDTH % 8 == 0) ? (EPD_7IN5B_V2_WIDTH / 8 ) : (EPD_7IN5B_V2_WIDTH / 8 + 1)) * EPD_7IN5B_V2_HEIGHT;
+  if ((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
+    printf("Failed to apply for black memory...\r\n");
+    while(1);
+  }
+  if ((RYImage = (UBYTE *)malloc(Imagesize)) == NULL) {
+    printf("Failed to apply for red memory...\r\n");
+    while(1);
+  }
+  printf("NewImage:BlackImage and RYImage\r\n");
+  Paint_NewImage(BlackImage, EPD_7IN5B_V2_WIDTH, EPD_7IN5B_V2_HEIGHT , 0, WHITE);
+  Paint_NewImage(RYImage, EPD_7IN5B_V2_WIDTH, EPD_7IN5B_V2_HEIGHT , 0, WHITE);
+
   initWiFi();
   delay(10000);
   if (WiFi.status() !=WL_CONNECTED) {
-    nocon = nocon+1
+    nocon = nocon+1;
+    Paint_SelectImage(BlackImage);
+    Paint_DrawString_EN(10, 20, SSID, &Font12, WHITE, BLACK);
+    Paint_DrawString_EN(10, 20, MAC, &Font12, WHITE, BLACK);
   }
 }
 
