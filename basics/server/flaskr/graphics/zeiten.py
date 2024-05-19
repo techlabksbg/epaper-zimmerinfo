79771e94e951dd321f0ac05712b5c4803f88c48d
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2024-05-19 16:21:27 +0200

    more type annotation removal

--- basics/server/flaskr/graphics/zeiten.py
+++ basics/server/flaskr/graphics/zeiten.py
@@ -12,21 +12,21 @@ separatorenKSBG[2]=separatorenKSBG[4]=separatorenKSBG[6]=1
 anfangszeitenISME = [time(t[0],t[1]) for t in [
         [7,45], [8,40], [9,35], [10,40], [11,35], [12,25], [13,15], [14,5], [14,55]]]
 
-rasterZeiten:list[time] = [time(t[0],t[1]) for t in [
+rasterZeiten:list = [time(t[0],t[1]) for t in [
     [7,40], [8,34], [9,28], [10,30], [11,24], 
     [12,14], [13,4], 
     [13,55], [14,49], [15,43], [16,33], [17,23],
     [18,15], [19,5], [19,55]]]
 
 
-def rasterTextKSBG() -> list[list[str]]:
+def rasterTextKSBG() -> list:
     r = []
     for s in rasterZeiten:
         e = addDelta(s,timedelta(minutes=45))        
         r.append([s.strftime("%H:%M"), e.strftime("- %H:%M")])
     return r
 
-def rasterTextISME() -> list[list[str]]:
+def rasterTextISME() -> list:
     r = []
     for s in anfangszeitenISME:
         e = addDelta(s,timedelta(minutes=45))        
